293a53bcac9555d02aac9a6e27c03d470632378d
LoC <locpython@gmx.de>
2020-06-18 21:57:51 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+from discord.ext.commands import Bot, Context, CommandError, guild_only, CommandNotFound
+import discord.utils
+import discord
+
+bot = Bot(command_prefix="|", case_insensitive=True)
+
+
+@bot.event
+async def on_ready():
+    print("Corona ist doof")
+
+
+@bot.command(name="get")
+async def get(ctx):
+    resp = requests.get("https://api.covid19api.com/summary", params=dict())
+
+    data = resp.json()
+
+    newcases = data["Countries"][63]["NewConfirmed"]
+    totalcases = data["Countries"][63]["TotalConfirmed"]
+
+    newrecs = data["Countries"][63]["NewRecovered"]
+    totalrecs = data["Countries"][63]["TotalRecovered"]
+
+    newdeaths = data["Countries"][63]["NewDeaths"]
+    totaldeaths = data["Countries"][63]["TotalDeaths"]
+
+    embed = discord.Embed()
+
+
+
+bot.run("NzIzMTU1NTUxMDAwNzIzNTY5.XuthnQ.0quqAfT0qKzjpjSeji7Rgb6MfPY")
