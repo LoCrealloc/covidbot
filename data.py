88584e80bc29ea0389e95b412de9accdd9186f3e
LoC <locpython@gmx.de>
2020-06-19 21:04:41 +0200

    data.py added, added Embed to main.py

---
+++ data.py
@@ -0,0 +1,4 @@
+thumb_url = "https://cdn.discordapp.com/avatars/723155551000723569/e67c05562c656f4bfc1af8d44f930676.png"
+
+version = "0.0.2"
+
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import requests
-from discord.ext.commands import Bot, Context, CommandError, guild_only, CommandNotFound
+from discord.ext.commands import Bot
 import discord.utils
 import discord
 import data
+from token import token
 
 bot = Bot(command_prefix="|", case_insensitive=True)
 
 
 @bot.event
 async def on_ready():
-    print("Corona ist doof")
+    print("Bot is online")
+    status = discord.Game("https://github.com/LoCrealloc/covidbot")
+    await bot.change_presence(status=discord.Status.online, activity=status)
 
 
 @bot.command(name="get")
-async def get(ctx):
+async def get(ctx: discord.Message):
     resp = requests.get("https://api.covid19api.com/summary", params=dict())
 
     daten = resp.json()
@@ -33,26 +36,27 @@ async def get(ctx):
 
     embed = discord.Embed(title="Corona-Statistiken",
                           description="Aktuelle Statistiken zur Corona-Krise",
-                          color="#0404B4",
-                          url="https://github.com/LoCrealloc/covidbot",
+                          color=0x088A08,
+                          url="https://covid19api.com",
                           )
 
-    embed.set_thumbnail(url=daten.thumb_url)
+    embed.set_thumbnail(url=data.thumb_url)
 
-    embed.add_field(name="Fälle weltweit", value=wwcases, inline=False)
+    embed.add_field(name="Fälle weltweit", value=wwcases, inline=True)
     embed.add_field(name="Genesene weltweit", value=wwrecs, inline=True)
     embed.add_field(name="Tote weltweit", value=wwdeaths, inline=True)
 
-    embed.add_field(name="Neue Fälle Deutschland", value=denewcases, inline=False)
+    embed.add_field(name="Neue Fälle Deutschland", value=denewcases, inline=True)
     embed.add_field(name="Neue Genesene Deutschland", value=denewrecs, inline=True)
     embed.add_field(name="Neue Tote Deutschland", value=denewdeaths, inline=True)
 
-    embed.add_field(name="Gesamt Fälle Deutschland", value=detotalcases, inline=False)
+    embed.add_field(name="Gesamt Fälle Deutschland", value=detotalcases, inline=True)
     embed.add_field(name="Gesamt Genesene Deutschland", value=detotalrecs, inline=True)
     embed.add_field(name="Gesamt Tote Deutschland", value=detotaldeaths, inline=True)
 
     embed.add_field(name="Daten", value="Diese Daten stammen von https://covid19api.com", inline=False)
 
-    await ctx.channel.send(content="@" + str(ctx.author), embed=embed)
+    await ctx.channel.send(embed=embed)
 
-bot.run(data.token)
+
+bot.run(token)
